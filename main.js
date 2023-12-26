f7b903db10e47895d200803cd516b93826ed4787
AlexisJamp <154271476+AlexisJamp@users.noreply.github.com>
2023-12-26 17:02:01 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+
+let first = prompt("Plese enter first number: ");
+let second = prompt("Please enter second number: ");
+
+let plus = Number(first) + Number(second);
+let minus = Number(first) - Number(second);
+let multiply = Number(first) * Number(second);
+let divide = Number(first) / Number(second);
+
+alert(`User entered ${first} and ${second} = ${plus}`);
+alert(`User entered ${first} and ${second} = ${minus}`);
+alert(`User entered ${first} and ${second} = ${multiply}`);
+alert(`User entered ${first} and ${second} = ${divide}`);
